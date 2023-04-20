5543cb4d83e172cdefd68f0822c9398c7f65ba62
Lip <philippe.roquesgeoffroy@gmail.com>
2023-04-21 00:13:23 +0200

    Logo + themes

---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import styles from './Content.module.scss';
+
+function Content() {
+    return (<div className="flex-fill container p-20">
+    <h1 className='my-30'>Nos recettes</h1>
+    <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+                <div className={styles.elem}>element</div>
+                <div className={styles.elem}>element</div>
+                <div className={styles.elem}>element</div>
+                <div className={styles.elem}>element</div>
+                <div className={styles.elem}>element</div>
+                <div className={styles.elem}>element</div>
+            </div>
+        </div>
+    </div>);
+}
+
+
+export default Content;
